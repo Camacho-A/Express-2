a4c5deec6504f6402a603d945154a9e73ab1efce
Angelica Camacho <113385869+Camacho-A@users.noreply.github.com>
2022-11-06 13:29:04 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -9,10 +9,30 @@ app.get("/greeting", (request, response) => {
 	response.send("Hello, stranger")
 })
 
-// ----- route with param --------
+// ----- Greeting route with param --------
 app.get("/greeting/:name", (request, response) => {
 	response.send(`What's up ${request.params.name}`)
 })
+ 
+// ---- Tip calculator ------
+app.get("/tip/:total/:tipPercentage", (request, response) => {
+	const total = parseInt(request.params.total)
+	const tipPercentage = parseInt(request.params.tipPercentage)
+	const tip = .01
+	response.send(`tip total ${(tipPercentage * tip) * total}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ---------- Listen pn port ------------
 app.listen(process.env.PORT, () => {
